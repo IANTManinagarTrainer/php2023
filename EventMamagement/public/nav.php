1cbc189625dd7e3d680652fb11bc2062afc14284
Nikhil Gupta <gnikhil@iant.com>
2023-07-02 17:28:09 +0530

    Public Pages Added

---
+++ EventMamagement/public/nav.php
@@ -0,0 +1,7 @@
+<div class="bg-dark">
+    <a class="ms-3 btn btn-dark" href="home.php">Home</a>
+    <a class="btn btn-dark" href="about.php">About</a>
+    <a class="btn btn-dark" href="contact.php">Contact Us</a>
+    <a class="float-end me-5 btn btn-dark" href="login.php">Log In</a>
+    <a href="signup.php" class="float-end btn btn-danger"> Sign Up</a>
+</div>
